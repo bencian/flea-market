d6b8d6efd4610f0fbdef029c32591180db70294b
Ciancio, Benjamin <bencian94@gmail.com>
2019-10-31 12:21:57 -0300

    added RSpec gem, directory and configuration

--- Gemfile
+++ Gemfile
@@ -56,6 +56,12 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'rails-controller-testing'
+  gem 'rspec-rails'
+  gem 'shoulda-matchers', '~> 3.1.2'
+  gem 'should_not'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module FleaMarket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -15,5 +17,9 @@ module FleaMarket
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |generator|
+      generator.test_framework :rspec
+    end
   end
 end
