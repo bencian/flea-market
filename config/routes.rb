e297ead62bd383f55cf98e397017215c15799314
Ciancio, Benjamin <bencian94@gmail.com>
2019-11-04 09:15:32 -0300

    routing for products fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :products
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "frontend/main#index"
 
@@ -9,6 +6,7 @@ Rails.application.routes.draw do
     root to: "backend/main#index", as: 'admin_root'
     resources :categories, :module => 'backend'
     resources :tags, :module => 'backend'
+    resources :products, :module => 'backend'
   end
   devise_for :admin, controllers: {
     sessions: 'admins/sessions',
