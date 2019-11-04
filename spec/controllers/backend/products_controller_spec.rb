c279c8994b529849d76e7be923d21507c0d9e03d
Ciancio, Benjamin <bencian94@gmail.com>
2019-11-04 09:14:31 -0300

    Products resource generated

---
+++ spec/controllers/backend/products_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Backend::ProductsController, type: :controller do
+
+end
---
+++ spec/decorators/backend/product_decorator_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe Backend::ProductDecorator do
+end
