869d976ab156e6cf0fb5d23f3a71703c8b9cd0ca
Ciancio, Benjamin <bencian94@gmail.com>
2019-11-06 09:00:10 -0300

    product crud finished

--- app/presenters/products_presenter.rb
+++ app/presenters/products_presenter.rb
@@ -16,7 +16,6 @@ class ProductsPresenter
   private
 
   def filter_params
-    byebug
     @params[:products_filter] ? @params.require(:products_filter).permit(:query,:category_id) : {}
   end
 end
