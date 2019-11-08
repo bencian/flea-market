012247858e3725ff4b50536994d40831bd017eb3
matias conde <matiascondemartinez1997@gmail.com>
2019-11-08 10:23:05 -0300

    seed of categories and tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 Admin.create(username: 'admin', email: 'admin@snappler.com', password: '123123', password_confirmation: '123123')
 FrontendConfiguration.create(singleton_guard: 0)
+10.times {
+  Tag.create(name: Faker::Name.unique.name)
+}
+
+10.times {
+  Category.create(name: Faker::Company.unique.bs)
+}
+
