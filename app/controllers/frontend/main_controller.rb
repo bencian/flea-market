b1e906e12932bfdf6a586c5ed9e456b0ccfb6da0
matias conde <matiascondemartinez1997@gmail.com>
2019-11-08 16:03:43 -0300

    filters for products in frontend

--- app/controllers/frontend/main_controller.rb
+++ app/controllers/frontend/main_controller.rb
@@ -2,7 +2,7 @@ module Frontend
   class MainController < FrontendController
     def index
       @sliders = Slider.all
-      @products = Product.limit(4).order('RAND()')
+      @products = Product.limit(4).order('RAND()').decorate
     end
   end
 end
--- app/filters/products_filter.rb
+++ app/filters/products_filter.rb
@@ -1,19 +1,28 @@
 class ProductsFilter
   include ActiveModel::Model
 
-  attr_accessor :query, :category_id
-
-  def initialize(params)
-    @query = params[:query]
-    @category_id = params[:category_id]
-    @only_active = params[:only_active]
-  end
+  attr_accessor :query, :category_id, :order_by, :only_active
 
   def call
     products = Product.all
-    products = products.with_name(@query) unless @query.blank?
-    products = products.with_category(@category_id) unless @category_id.blank?
-    products = products.where(active: true) if @only_active
+    products = products.with_name(query)           unless query.blank?
+    products = products.with_category(category_id) unless category_id.blank?
+    products = products.where(active: true)        if only_active
+    products = products.order(order_field)
+
     products
+  end
+
+  private
+
+  def order_field
+    case order_by
+    when 'asc'
+      'name ASC'
+    when 'desc'
+      'name DESC'
+    else
+      'name ASC'
+    end
   end
 end
