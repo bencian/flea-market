06f9262a3648797fff90c77c52c564e85882cdfb
matias conde <matiascondemartinez1997@gmail.com>
2019-11-11 10:42:01 -0300

    internationalization of many many things

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,10 +1,6 @@
 class Tag < ApplicationRecord
   # -- Relations
-<<<<<<< HEAD
-  has_and_belongs_to_many :products, dependent: :restrict_with_error, optional: true
-=======
   has_and_belongs_to_many :products, optional: true
->>>>>>> 55afca700ab49e9237d0e45fcd085e00718e8009
 
   # -- Validations
   validates :name, presence: true, uniqueness: true
